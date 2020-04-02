be84a8838ce4aa15f09b6e29cfedd05778ba8064
Tiago Baptista Coelho <tiagojnbaptistacoelho@gmail.com>
2020-04-02 22:53:52 +0100

    fetched quote with axios

--- src/App.js
+++ src/App.js
@@ -1,19 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Navbar from './components/Navbar';
 import QuoteCard from './components/QuoteCard';
-import QuoteList from './components/QuoteList';
+import './App.css'
+import axios from 'axios';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state= {
+      simpson:[],
+    }
+  }
+
+  componentDidMount() {
+    const url = "https://simpsons-quotes-api.herokuapp.com/quotes";
+    fetch(url)
+        .then(response => response.json())
+        .then(initialQuote=> {
+            this.setState({
+                simpson:initialQuote[0]
+            });
+    });
+};
+
+  getNewQuote = (event) => {
+    const url = 'https://simpsons-quotes-api.herokuapp.com/quotes';
+    axios.get(url)
+    .then(response => response.data)
+    .then(newQuote => {
+      this.setState({
+        simpson:newQuote[0],
+      });
+    })
+  
+  }
 
 
-function App() {
+render(){
   return (
     <div className="App">
         <Navbar />
-        <QuoteCard quote="Ah, be creative. Instead of making sandwhiches with bread, use Pop-Tarts. Instead of chewing gum, chew bacon." character="Dr. Nick" image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNickRiviera.png?1497567511084" />
-        <QuoteCard quote= "Shoplifting is a victimless crime, like punching someone in the dark." character="Nelson Muntz" image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72" />
+        <div className='container'>
+          <QuoteCard {...this.state.simpson}/>
+          <button onClick={this.getNewQuote}>New quote</button>
+        </div>
         
-        <QuoteList />
     </div>
   );
+}
 }
 
 export default App;
